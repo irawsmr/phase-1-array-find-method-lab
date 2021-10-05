7e432fc02d07f4faa6bda78a896d6e1a07a1fe74
Joshua Berliner <joshuaberliner@gmail.com>
2021-10-04 22:45:50 -0400

    commit tree 1e446013bcf8dbe25e4949171df175892260ecde

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function isW(element){
+    return element.result === "W";
+    
+}
 
+function superbowlWin(objArray){
+    let obj = objArray.find(o => o.result === "W");
+    //console.log(obj);
+    if (obj === undefined) return undefined;
+    else return obj.year;
+}
